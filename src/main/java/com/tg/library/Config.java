f5959cec6c051a2ef8ab2cd95f60a458d246de03
TomaszGerono <tomasz.gerono@gmail.com>
2025-12-03 13:29:56 +0100

    create config class

---
+++ src/main/java/com/tg/library/Config.java
@@ -0,0 +1,19 @@
+package com.tg.library;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tomasz
+ */
+public class Config {
+    
+    private static final String users_db_url = "jdbc:sqlite:./sqlite/db/users.db";
+    private static final String library_db_url = "jdbc:sqlite:./sqlite/db/library.db";
+    
+    
+    
+}
